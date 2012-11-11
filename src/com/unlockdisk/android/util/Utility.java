97be5331896073cdd122bce44e7991467d79b16f
manjeet <sudhanshu.manjeet@gmail.com>
2012-11-12 00:17:45 +0530

    added eclipse proj file

--- src/com/unlockdisk/android/util/Utility.java
+++ src/com/unlockdisk/android/util/Utility.java
@@ -1,10 +1,5 @@
 package com.unlockdisk.android.util;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import com.unlockdisk.android.opengl.MainGLActivity;
 
 public class Utility {
